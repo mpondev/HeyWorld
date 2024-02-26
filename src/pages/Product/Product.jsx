e807566a01ac6e7f0e1950342080a15591628156
Mario Ponce <mario@mpondev.com>
2024-02-26 22:30:15 +0100

    Modify and style Product page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,11 +1,29 @@
 import PageNav from '../../components/PageNav/PageNav.jsx';
+import styles from './Product.module.css';
 
 function Product() {
   return (
-    <div>
+    <main className={styles.product}>
       <PageNav />
-      <h1>Product</h1>
-    </div>
+
+      <section>
+        <img src="img-1.webp" alt="mountains with lake" />
+        <div>
+          <h2>About HeyWorld</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est
+            dicta illum vero culpa cum quaerat architecto sapiente eius non
+            soluta, molestiae nihil laborum, placeat debitis, laboriosam at fuga
+            perspiciatis?
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            doloribus libero sunt expedita ratione iusto, magni, id sapiente
+            sequi officiis et.
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
 
