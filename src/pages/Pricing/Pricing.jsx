d3bdcae13301d85ba339dcb8dc3b22be14f4d1af
Mario Ponce <mario@mpondev.com>
2024-02-26 22:37:12 +0100

    Modify and style Pricing page

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,11 +1,29 @@
 import PageNav from '../../components/PageNav/PageNav.jsx';
 
+// Uses same styles as Product
+import styles from '../Product/Product.module.css';
+
 function Pricing() {
   return (
-    <div>
+    <main className={styles.product}>
       <PageNav />
-      <h1>Pricing</h1>
-    </div>
+
+      <section>
+        <div>
+          <h2>
+            Simple pricing. <br /> Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam,
+            ipsa necessitatibus asperiores distinctio accusamus nihil. Tempora,
+            quia nulla hic blanditiis deleniti distinctio consequatur recusandae
+            odio vero consectetur animi, quod ipsa!
+          </p>
+        </div>
+
+        <img src="img-2.webp" alt="Town with mounatins in the background" />
+      </section>
+    </main>
   );
 }
 
