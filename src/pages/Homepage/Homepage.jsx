9a6f1e4444d847c280b8d8530e6239ab8214526c
Mario Ponce <mario@mpondev.com>
2024-02-26 22:14:42 +0100

    Modify and style Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,26 @@
+import { Link } from 'react-router-dom';
+
 import PageNav from '../../components/PageNav/PageNav.jsx';
+import styles from './Homepage.module.css';
 
 function Homepage() {
   return (
-    <div>
+    <main className={styles.homepage}>
       <PageNav />
-      <h1>HeyWorld</h1>
-    </div>
+      <section>
+        <h1>
+          You travel the world <br /> HeyWorld keeps track of your adventures
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experience, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to="/app" className="cta">
+          Start tracking now
+        </Link>
+      </section>
+    </main>
   );
 }
 
