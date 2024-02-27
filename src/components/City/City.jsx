7bd2626b981001bed6276e040c8b8e0d9b47d5df
Mario Ponce <mario@mpondev.com>
2024-02-27 17:44:42 +0100

    Add reading/setting query strings capability

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -1,7 +1,10 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 function City() {
   const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
 
   return <h1>City {id}</h1>;
 }
--- src/components/CityItem/CityItem.jsx
+++ src/components/CityItem/CityItem.jsx
@@ -12,9 +12,12 @@ const formatDate = date =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, date, emoji, id } = city;
+  const { cityName, date, emoji, id, position } = city;
   return (
-    <Link className={styles.cityItem} to={`${id}`}>
+    <Link
+      className={styles.cityItem}
+      to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+    >
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>({formatDate(date)})</time>
