ab6676b391480dcf8cc2a7061fa482cc82d8aeed
Mario Ponce <mario@mpondev.com>
2024-03-03 19:37:08 +0100

    API. Add env variables

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 
 // START SERVER
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });
