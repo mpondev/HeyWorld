784dd9bbc77a40cd4c77403edb5e064c3ba2306b
Mario Ponce <mario@mpondev.com>
2024-03-03 13:11:14 +0100

    Start API. Handling GET requests

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 // MIDDLEWARES
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.json());
+// app.use(express.static(`${__dirname}/public`));
 
-app.get('/message', (req, res) => {
-  res.json({ message: 'Hello from the server!' });
+const cities = JSON.parse(fs.readFileSync(`${__dirname}/data/cities.json`));
+
+app.get('/api/v1/cities', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: cities.length,
+    data: {
+      cities,
+    },
+  });
 });
 
 module.exports = app;
