7c857766c895c21b653de31841b23d67df3668a5
Mario Ponce <mario@mpondev.com>
2024-03-03 19:30:55 +0100

    API. Serving static files

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 // MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
-// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
 app.use('/api/v1/cities', cityRouter);
