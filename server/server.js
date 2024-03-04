dbee67f108c9586c87d07792521ed6ec9476cc7f
mpondev <mario@mpondev.com>
2024-03-04 10:35:53 +0100

    API. Connect to MongoDB with mongoose and fix .env gitignore

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {}).then(con => {
+  console.log(con.connection);
+  console.log('DB connection successful!');
+});
 
 // START SERVER
 const PORT = process.env.PORT || 3000;
