5a4a80873a425849589af6f0c16fef204aa73992
Mario Ponce <mario@mpondev.com>
2024-03-04 16:44:31 +0100

    API. Refactor and improve Error Handling

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const globalErrorHandle = require('./controllers/errorController.js');
+const AppError = require('./utils/appError.js');
 const cityRouter = require('./routes/cityRoutes.js');
 const userRouter = require('./routes/userRoutes.js');
 
@@ -19,27 +21,10 @@ app.use('/api/v1/users', userRouter);
 
 // Handle unhandle routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
 });
 
 // Global Error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandle);
 
 module.exports = app;
