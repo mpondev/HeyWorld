699447c0c77886c3bf82ec01a7883dee2c3f6ff1
Mario Ponce <mario@mpondev.com>
2024-05-23 07:22:35 +0200

    Add hamburger menu for screen devices

--- client/src/components/PageNav/PageNav.jsx
+++ client/src/components/PageNav/PageNav.jsx
@@ -1,10 +1,15 @@
+import { useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Squash as Hamburger } from 'hamburger-react';
 
 import Logo from '../Logo/Logo.jsx';
 
 import styles from './PageNav.module.css';
 
 function PageNav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null);
+
   return (
     <nav className={styles.nav}>
       <Logo />
@@ -22,6 +27,32 @@ function PageNav() {
           </NavLink>
         </li>
       </ul>
+
+      <div ref={ref} className={styles.hamburger}>
+        <Hamburger
+          toggled={isOpen}
+          toggle={setIsOpen}
+          size={30}
+          label="Show menu"
+        />
+        {isOpen && (
+          <div className={styles['hamburger-menu']}>
+            <ul className={styles['hamburger-links']}>
+              <li>
+                <NavLink to="/product">Product</NavLink>
+              </li>
+              <li>
+                <NavLink to="/pricing">Pricing</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login" className={styles.ctaLink}>
+                  Login
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
